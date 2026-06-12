5f046a76dcac6a8f6f351f70929207b1e9912e48
EiNandarKhaing21 <einandarkhaing@my.jcu.edu.au>
2026-06-12 11:46:44 +0800

    Answer questions and write code to produce a random number between 1 and 100 inclusive.

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+#What did you see on line 1?
+#11, 11, 9, 10, 14, 18, 12, 20, 6, 7
+#What was the smallest number you could have seen, what was the largest?
+# The smallest number is 6 and the largest one is 20.
+
+#What did you see on line 2?
+#7, 9, 9, 3, 9, 5, 7, 3, 5, 5
+#What was the smallest number you could have seen, what was the largest?
+#The smallest number is 3 and the largest one is 9.
+#Could line 2 have produced a 4?
+#No
+
+#What did you see on line 3?
+#3.7935733646535787, 4.534356007901735, 4.289322593621626, 3.256785844070113, 3.528045992512311, 4.176633308655742, 3.0310597859016264, 4.6433713046489, 2.642778341717931, 3.4984305998963925
+#What was the smallest number you could have seen, what was the largest?
+#The smallest number is 2.642778341717931 and the largest one is 4.6433713046489.
+
+print(random.randint(1, 101))
