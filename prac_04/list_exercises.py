1258d00d32d2fb941c7801890b94926ce8f10ab9
EiNandarKhaing21 <einandarkhaing@my.jcu.edu.au>
2026-07-09 17:57:18 +0800

    Finish the list exercises

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,15 @@
+numbers = []
+counter = 0
+while counter < 5:
+    try:
+        number = int(input("Number: "))
+        numbers.append(number)
+        counter += 1
+    except ValueError:
+        print("Input must be a valid number!")
+
+print(f"The first number is {numbers[0]}.")
+print(f"The last number is {numbers[4]}.")
+print(f"The smallest number is {min(numbers)}.")
+print(f"The largest number is {max(numbers)}.")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}.")
