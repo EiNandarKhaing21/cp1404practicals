70afd4951df48a4ca9476f3f7b993178799ec206
EiNandarKhaing21 <einandarkhaing@my.jcu.edu.au>
2026-07-17 21:27:51 +0800

    Complete display_projects function

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,8 +1,8 @@
 class ProgrammingLanguage:
-    def __init__(self, name="", typing="", reflection="", year=""):
+    def __init__(self, name="", typing="", is_reflection=False, year=""):
         self.name = name
         self.typing = typing
-        self.reflection = reflection
+        self.is_reflection = is_reflection
         self.year = year
 
     def is_dynamic(self):
